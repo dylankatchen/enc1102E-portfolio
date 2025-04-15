dfe2a6a98020fb25aa2f5d91367a2263d42f3928
Dylan Katchen <89935431+dylankatchen@users.noreply.github.com>
2025-04-15 19:59:55 -0400

    start of website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  <>
+  {/* Header */}
+  <section className='bg-gray-800 text-white'>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Dylan Katchen
+      </h1>
+      <h2 className="text-2xl font-bold">
+        E-Portfolio
+      </h2>
+    </div>
+
+  </section>
+  {/* Main */}
+  <section>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+    </section>
+    {/*dropdowns of course goals*/}
+    <section></section>
+  </>
+}
+
+export default App
